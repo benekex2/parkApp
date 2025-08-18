1193ff188b36da535c72534114d0c7de42bdd759
Krzysztof Białogłowicz <krzysztof@bialoglowicz.pl>
2025-08-18 10:42:54 +0200

    Add basic setup

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
+
+module.exports = createJestConfig(customJestConfig);
